3d9094035e7ebaf8df7b2debc750494aeb12195e
juancin10xd <jcgpalacios1@gmail.com>
2024-10-23 12:18:20 -0500

    iniciando repositorio Spring

---
+++ src/main/java/pe/com/cibertec/Lp2EfGorritiJuanCarlosApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2EfGorritiJuanCarlosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2EfGorritiJuanCarlosApplication.class, args);
+	}
+
+}
